6a80bbfea8489d613102faef8fa36af8935f2259
Saharat Paynok <saharat.paynok@gmail.com>
2022-07-15 00:27:00 +0700

    Add project and client relationship

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,10 +5,15 @@ const ProjectType = new GraphQLObjectType({
   name: 'Project',
   fields: () => ({
     id: { type: GraphQLID },
-    clientId: { type: GraphQLID },
     name: { type: GraphQLString },
     description: { type: GraphQLString },
     status: { type: GraphQLString },
+    client: {
+      type: ClientType,
+      resolve(parent, args) {
+        return clients.find((c) => parent.clientId === c.id);
+      },
+    },
   }),
 });
 
