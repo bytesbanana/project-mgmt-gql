c0dc5279214543cefec8ab65c907d303cdd26d85
Saharat Paynok <saharat.paynok@gmail.com>
2022-07-15 01:09:13 +0700

    Add mongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const connectDB = require('./config/db');
 
 const schema = require('./schema/schema');
 const port = process.env.PORT || 5000;
 const app = express();
+
+connectDB();
 
 app.use(
   '/graphql',
